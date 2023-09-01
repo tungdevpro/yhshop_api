bd4792f40fe55f2879de82badd043de1e8da117a
tungdevpro <tungdo21899@gmail.com>
2023-09-01 22:59:12 +0700

    fear(auth): create error constanst

--- modules/auth/entity/error.go
+++ modules/auth/entity/error.go
@@ -1,3 +1,12 @@
 package entity
 
+import "errors"
+
+
+var (
+	ErrCannotCreateUser = errors.New("Cannot create user")
+	ErrEmailCannotBeBlank = errors.New("Email cannot be black")
+	ErrPasswordCannotBeBlank = errors.New("Password cannot be black")
+)
+
 
