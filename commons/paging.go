450aa59c9c4351f9b2a4389e671b888f1eedc2b7
tungdevpro <tungdo21899@gmail.com>
2023-09-05 08:15:03 +0700

    hotfix* remove bson tag

--- commons/paging.go
+++ commons/paging.go
@@ -1,30 +1,17 @@
 package commons
 
 type Paging struct {
-		Page  int   `bson:"page" json:"page" form:"page"`
-		Limit int   `bson:"limit" json:"limit" form:"limit"`
-		Total int64 `bson:"total" json:"total" form:"-"`
+	Page  int   `json:"page" form:"page"`
+	Limit int   `json:"limit" form:"limit"`
+	Total int64 `json:"total" form:"-"`
 }
 
 func (p *Paging) Process() {
 	if p.Page <= 0 {
-			p.Page = 1
+		p.Page = 1
 	}
 
 	if p.Limit <= 0 || p.Limit >= 10 {
 		p.Limit = 10
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
