cb93f7da6a4a8916441e162a544b4c6b75beadcb
tungdevpro <tungdo21899@gmail.com>
2023-09-19 16:11:31 +0700

    fix(auth_required): deny check token when call api auth

--- middleware/auth_required.go
+++ middleware/auth_required.go
@@ -2,18 +2,31 @@ package middleware
 
 import (
 	"coffee_api/commons"
+	"coffee_api/configs/prefix"
 	"coffee_api/helpers"
 	jwtexplore "coffee_api/middleware/jwt_explore"
 	"errors"
 	"fmt"
 	"net/http"
+	"slices"
 
 	"github.com/gin-gonic/gin"
 	"github.com/indrasaputra/hashids"
 )
 
+var (
+	AllowPaths = []string{
+		fmt.Sprintf("%s%s%s", prefix.V1, prefix.Auth, prefix.Register),
+		fmt.Sprintf("%s%s%s", prefix.V1, prefix.Auth, prefix.Login),
+	}
+)
+
 func AuthRequired(appCtx commons.AppContext, bizJwt jwtexplore.Business) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
+		if slices.Contains(AllowPaths, ctx.FullPath()) {
+			ctx.Next()
+			return
+		}
 		tokenString := helpers.GetTokenFromAuthHeader(ctx.GetHeader("Authorization"))
 
 		uid, err := Validate(appCtx.Cfg, tokenString)
