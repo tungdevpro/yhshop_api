28c81edb834879458e8e497f39248710268723bc
tungdevpro <tungdo21899@gmail.com>
2023-09-22 11:54:01 +0700

    fix: check role for user

--- middleware/check_permission.go
+++ middleware/check_permission.go
@@ -1,3 +1,16 @@
 package middleware
 
-func CheckPermission() {}
+import (
+	"coffee_api/commons"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CheckPermission(ctx *gin.Context, role *commons.RoleAllowed) bool {
+	if role == nil {
+		return false
+	}
+
+	r := ctx.MustGet(commons.CurrentUser).(commons.Requester)
+	return r.GetRole() == string(*role)
+}
