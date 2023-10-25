24735f94c333600b13f215302f51ab81140565f5
tungdevpro <tungdo21899@gmail.com>
2023-10-25 10:28:08 +0700

    fix(auth): only return id when successful registration

--- modules/auth/transport/rest/rest_api.go
+++ modules/auth/transport/rest/rest_api.go
@@ -1,10 +1,11 @@
 package rest
 
 import (
+	"net/http"
+
 	"coffee_api/commons"
 	"coffee_api/modules/auth"
 	"coffee_api/modules/auth/entity"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -29,15 +30,15 @@ func (api *api) RegisterHandler() gin.HandlerFunc {
 		}
 
 		result, err := api.biz.Register(ctx.Request.Context(), &data)
-
 		if err != nil {
 			ctx.AbortWithStatusJSON(http.StatusBadRequest, commons.NewAppError(-1, err.Error()))
 			return
 		}
 
-		ctx.JSON(http.StatusOK, commons.CreateNewSuccessResp(result, entity.MsgCreateNewUser))
+		ctx.JSON(http.StatusOK, commons.CreateNewSuccessResp(result.Uid, entity.MsgCreateNewUser))
 	}
 }
+
 func (api *api) LoginHandler() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		var dto entity.LoginDTO
