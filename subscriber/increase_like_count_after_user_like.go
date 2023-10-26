b301358789306b584fe532e4aab014edff377827
tungdevpro <tungdo21899@gmail.com>
2023-10-26 13:39:39 +0700

    fix: remove comment code

--- subscriber/increase_like_count_after_user_like.go
+++ subscriber/increase_like_count_after_user_like.go
@@ -4,10 +4,8 @@ import (
 	"coffee_api/commons"
 	implShop "coffee_api/modules/shop/repository/repo_impl"
 	"coffee_api/modules/shop_like/entity"
-	"fmt"
 
 	"context"
-	"log"
 )
 
 func IncreaseLikeCountAfterUserLike(appCtx commons.AppContext, ctx context.Context) {
@@ -18,10 +16,7 @@ func IncreaseLikeCountAfterUserLike(appCtx commons.AppContext, ctx context.Conte
 		for {
 			msg := <-c
 			data := msg.Data().(*entity.ShopLike)
-			err := impl.IncrementLikeCount(ctx, data.ShopId)
-			fmt.Println("err:>> ", err)
-			// storage.CreateUserLike(ctx, data.UserId, data.ShopId)
-			log.Println("Pushed:", msg.Data())
+			_ = impl.IncrementLikeCount(ctx, data.ShopId)
 		}
 	}()
 
